ccada49f401bbc6eb24232c039cd77e0ebd40639
Se7enB2st <ethanhe1992@gmail.com>
2016-07-17 23:52:32 +0000

    Fix bug where it can't translates between same unit

--- unit_converter.rb
+++ unit_converter.rb
@@ -1,6 +1,6 @@
 require 'rspec/autorun'
 
-#UnitConverter exercise - Create a unit converter using TDD
+#UnitConverter exercise from Thoughtbot Upcase
 #UnitConverter.new.convert(5, :cup, :liter) #=> 1.183
 #UnitConverter.new.convert(5, :cup, :gram) #=> DimensionMissmatchError!
 
@@ -17,23 +17,45 @@ class UnitConverter
   def convert
     Quantity.new(@initial_quantity.amount * conversion_factor(from: @initial_quantity.unit, to: @target_unit), @target_unit)
   end
-end
 
-private
 
-#NO need to test private method, cuz user can't call it
-CONVERSION_FACTOR = {
-  cup: {
-    liter: 0.236588
+  private
+
+  # NO need to test private method, cuz user can't call it
+  CONVERSION_FACTORS = {
+    liter: {
+      liter: 1,
+      cup: 4.226775,
+      pint: 2.11338
+    },
+    gram: {
+      gram: 1,
+      kilgram: 1000
+    }
   }
-}
 
-def conversion_factor(from:, to:)
-  CONVERSION_FACTOR[from][to] || raise(DimensionMissmatchError,"Cant convert from #{from} to #{to}!" )
+  # If common unit is found then convert the unit
+  # else return DimensionMissmatchError
+  def conversion_factor(from:, to:)
+    dimension = common_dimension(from, to)
+    if !dimension.nil?
+      CONVERSION_FACTORS[dimension][to] / CONVERSION_FACTORS[dimension][from]
+    else
+      raise(DimensionMissmatchError, "Cant convert from #{from} to #{to}!" )
+    end
+  end
+  
+  # Find the common dimension between 2 units
+  # Search through CONVERSION FACTORS and find the common unit(key) that is included in both unit
+  def common_dimension(from, to)
+    CONVERSION_FACTORS.keys.find do |canonical_unit|
+      CONVERSION_FACTORS[canonical_unit].keys.include?(from) && CONVERSION_FACTORS[canonical_unit].keys.include?(to)
+    end
+  end
 end
 
 
-#TESTS:
+# TESTS:
 
 describe "UnitConverter" do
   describe "#convert" do
@@ -45,6 +67,16 @@ describe "UnitConverter" do
 
       expect(result.amount).to be_within(0.001).of(1.183)
       expect(result.unit).to eq(:liter)
+    end
+
+    it "can convert between quantities of the same unit" do
+      cups = Quantity.new(7, :cup)
+      converter = UnitConverter.new(cups, :cup)
+
+      result = converter.convert
+
+      expect(result.amount).to be_within(0.001).of(7)
+      expect(result.unit).to eq(:cup)
     end
 
     it "raises an error if the two units are of different dimensions" do
