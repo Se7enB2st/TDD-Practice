ed8ec5be4c3fc47343f8a59e3b18b576c0c92d0a
Se7enB2st <ethanhe1992@gmail.com>
2016-07-17 22:57:50 +0000

    pass error message test

--- unit_converter.rb
+++ unit_converter.rb
@@ -4,6 +4,8 @@ require 'rspec/autorun'
 #UnitConverter.new.convert(5, :cup, :liter) #=> 1.183
 #UnitConverter.new.convert(5, :cup, :gram) #=> DimensionMissmatchError!
 
+DimensionMissmatchError = Class.new(StandardError)
+
 class UnitConverter
   def initialize(initial_amount, initial_unit, target_unit)
     @initial_amount = initial_amount
@@ -14,19 +16,22 @@ class UnitConverter
   def convert
     @initial_amount * conversion_factor(from: @initial_unit, to: @target_unit)
   end
-
-  private
-
-  CONVERSION_FACTOR = {
-    cup: {
-      liter: 0.236588
-    }
-  }
-
-  def conversion_factor(from:, to:)
-    CONVERSION_FACTOR[from][to]
-  end
 end
+
+private
+
+CONVERSION_FACTOR = {
+  cup: {
+    liter: 0.236588
+  }
+}
+
+def conversion_factor(from:, to:)
+  CONVERSION_FACTOR[from][to] || raise(DimensionMissmatchError,"Cant convert from #{from} to #{to}!" )
+end
+
+
+#TESTS:
 
 describe "UnitConverter" do
   describe "#convert" do
@@ -36,7 +41,7 @@ describe "UnitConverter" do
       expect(converter.convert).to be_within(0.001).of(1.183)
     end
 
-    xit "raises an error if the two units are of different dimensions" do
+    it "raises an error if the two units are of different dimensions" do
       converter = UnitConverter.new(5, :cup, :gram)
 
       # pass in a { block }to raise an error
